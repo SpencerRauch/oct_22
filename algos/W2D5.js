b55e092f4612a9b29d98a1b089c32c642e710076
Spencer Rauch <rauchs@gmail.com>
2022-10-07 08:57:32 -0700

    solutions

--- algos/W2D5.js
+++ algos/W2D5.js
@@ -54,3 +54,66 @@ console.log(updateInventory(newInv1, currInv1))
 console.log(updateInventory(newInv2, currInv2))
 console.log(updateInventory(newInv3, currInv3))
 
+
+// function updateInventory(newInv, currInv) {
+//   for (let newEntry of newInv) { //iterate over items to be added into inventory
+//     let match = false; //set a flag to keep track of if we find a matching item in our currInv
+//     for (let currEntry of currInv) { //inner loop: iterating over existing items in inventory
+//       if (currEntry['name'] == newEntry['name']) { //if we find a matching entry
+//         match = true; //set match to true
+//         currEntry['quantity'] += newEntry['quantity']; //update quantities in our inventory
+//         break; //stop the loop since we've found our match
+//       }
+//     }
+//     if (!match) { //item didn't exist in our currInv
+//       currInv.push(newEntry) //so we add it
+//     }
+//   }
+//   return currInv;
+// }
+
+
+// function updateInventory2(newInv, currentInv) {
+//   const currInvTable = {};
+
+//   for (let i = 0; i < currentInv.length; i++) {
+//     // save a reference to this currentInv item into our hash table object
+//     // so we can look it up with O(1) constant time
+//     currInvTable[currentInv[i].name] = currentInv[i];
+//   }
+
+//   for (let i = 0; i < newInv.length; i++) {
+//     const item = newInv[i];
+
+//     if (currInvTable.hasOwnProperty(item.name)) {
+//       // retrieve the currentInv obj reference
+//       let currentInvItem = currInvTable[item.name];
+
+//       // update the obj by reference
+//       currentInvItem.quantity += item.quantity;
+//     } else {
+//       currentInv.push(item);
+//     }
+//   }
+//   return currentInv;
+// }
+
+// function updateInventory(newInv, currInv) {
+//   if (newInv.length > 0) {
+//       for (var new_item of newInv) {
+//           // console.log(new_item)
+//           var flag = 0
+//               for (var old_item of currInv) {
+//                   if (new_item['name'] == old_item['name']) {
+//                       old_item['quantity'] += new_item['quantity']
+//                       flag = 1
+//                   }
+//               }
+//               if (flag == 0) {
+//                   currInv.push(new_item)
+//               }
+  
+//       }
+//   }
+//   return currInv
+// }
